a81b3ec5920fb561ca96fc5e19960e7f555ca7e8
Alexandre Garault <alexandre.garault@orange.com>
2023-02-09 13:11:44 +0100

    feat : add reservation model

--- src/types/Travel.ts
+++ src/types/Travel.ts
@@ -1,14 +1,16 @@
+import { Car } from "./User";
 
 export type Travel = {
     id:number;
     smoker:boolean;
     petAccepted:boolean;
     steps:[StepTravel];
+    car:Car;
 }
 
 export type StepTravel = {
     dateStart:string;
     townStart:string;
-    townEnd:String;
+    townEnd:string;
     place:number;
 }
---
+++ src/types/reservation.ts
@@ -0,0 +1,9 @@
+import { StepTravel } from "./Travel";
+import { User } from "./User";
+
+export type Reservation = {
+    id:number;
+    validated:boolean;
+    user:User;
+    stepTravel:StepTravel;
+}
